eaf9f1998fd65cfcb08b42c23080739123a87ee7
pandubaguspr <pandubaguspr@gmail.com>
2019-04-02 14:24:18 +0700

    travel-project1

---
+++ resources/views/admin/layout/base.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>Color Admin | Dashboard v2</title>
+	<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" />
+	<meta content="" name="description" />
+	<meta content="" name="author" />
+	
+	<!-- ================== BEGIN BASE CSS STYLE ================== -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,400,400italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" type="text/css" />
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/jquery-ui/jquery-ui.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/bootstrap/4.0.0/css/bootstrap.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/font-awesome/5.0/css/fontawesome-all.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/plugins/animate/animate.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/css/material/style.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/css/material/style-responsive.min.css') }}" rel="stylesheet" />
+	<link href="{{ asset('assets/css/material/theme/default.css') }}" rel="stylesheet" id="theme" />
+	<!-- ================== END BASE CSS STYLE ================== -->
+	
+	<!-- ================== BEGIN PAGE LEVEL CSS STYLE ================== -->
+    <link href="{{ asset('assets/plugins/jquery-jvectormap/jquery-jvectormap.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/bootstrap-calendar/css/bootstrap_calendar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/gritter/css/jquery.gritter.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/nvd3/build/nv.d3.css') }}" rel="stylesheet" />
+	<!-- ================== END PAGE LEVEL CSS STYLE ================== -->
+	
+	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="{{ asset('assets/plugins/pace/pace.min.js') }}"></script>
+	<!-- ================== END BASE JS ================== -->
+</head>
+<body>
+		<!-- header -->
+		@include('admin.layout.header')
+		
+		<!-- sidebar -->
+		@include('admin.layout.sidebar')
+		
+		<!-- content -->
+		@yield('content')
+		
+		<!-- begin scroll to top btn -->
+		<a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i class="material-icons">keyboard_arrow_up</i></a>
+		<!-- end scroll to top btn -->
+	</div>
+	<!-- end page container -->
+	
+	<!-- ================== BEGIN BASE JS ================== -->
+	<script src="{{ asset('assets/plugins/jquery/jquery-3.2.1.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/bootstrap/4.0.0/js/bootstrap.bundle.min.js') }}"></script>
+	<!--[if lt IE 9]>
+		<script src="../assets/crossbrowserjs/html5shiv.js"></script>
+		<script src="../assets/crossbrowserjs/respond.min.js"></script>
+		<script src="../assets/crossbrowserjs/excanvas.min.js"></script>
+	<![endif]-->
+	<script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/js-cookie/js.cookie.js') }}"></script>
+	<script src="{{ asset('assets/js/theme/material.min.js') }}"></script>
+	<script src="{{ asset('assets/js/apps.min.js') }}"></script>
+	<!-- ================== END BASE JS ================== -->
+	
+	<!-- ================== BEGIN PAGE LEVEL JS ================== -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.2/d3.min.js"></script>
+    <script src="{{ asset('assets/plugins/nvd3/build/nv.d3.js') }}"></script>
+    <script src="{{ asset('assets/plugins/jquery-jvectormap/jquery-jvectormap.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/jquery-jvectormap/jquery-jvectormap-world-merc-en.js') }}"></script>
+    <script src="{{ asset('assets/plugins/bootstrap-calendar/js/bootstrap_calendar.min.js') }}"></script>
+	<script src="{{ asset('assets/plugins/gritter/js/jquery.gritter.js') }}"></script>
+	<script src="{{ asset('assets/js/demo/dashboard-v2.min.js') }}"></script>
+	<!-- ================== END PAGE LEVEL JS ================== -->
+	
+	<script>
+		$(document).ready(function() {
+			App.init();
+			DashboardV2.init();
+		});
+	</script>
+	
+</body>
+</html>
